8e674443128bdc69b06f1e0eff355bc535d28817
Grischa Meyer <grischa@gmail.com>
2014-07-08 14:06:47 +1000

    first commit, basic setup

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pip install virtualenvwrapper
+source virtualenvwrapper.sh
+mkvirtualenv -a `pwd` salt-master -r requirements.txt
+rm $VIRTUAL_ENV/bin/postactivate
+ln -s `pwd`/utils/venv-postactivate $VIRTUAL_ENV/bin/postactivate
+rm $VIRTUAL_ENV/bin/predeactivate
+ln -s `pwd`/utils/venv-predeactivate $VIRTUAL_ENV/bin/predeactivate
+deactivate
+workon salt-master
